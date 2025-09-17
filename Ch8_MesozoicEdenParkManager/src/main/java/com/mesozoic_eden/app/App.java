bdbbcf5214751ace7ca348ae4d42ed1b3a20d8cc
Ignacio <ignacio_brittez@outlook.com>
2025-09-17 17:23:18 -0300

    feat(ch8): template project added

---
+++ Ch8_MesozoicEdenParkManager/src/main/java/com/mesozoic_eden/app/App.java
@@ -0,0 +1,13 @@
+package com.mesozoic_eden.app;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+
+    public void start() {
+        System.exit(0);
+    }
+
+}
