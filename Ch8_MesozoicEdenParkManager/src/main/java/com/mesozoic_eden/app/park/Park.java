991a881eaf2f094ca602ce497e7dd232b9c1366e
Ignacio <ignacio_brittez@outlook.com>
2025-09-17 17:25:12 -0300

    feat(ch8): Park class created

---
+++ Ch8_MesozoicEdenParkManager/src/main/java/com/mesozoic_eden/app/park/Park.java
@@ -0,0 +1,38 @@
+package com.mesozoic_eden.app.park;
+
+public class Park {
+    private String name;
+    private boolean isParkOpen;
+
+    // private DinosaurManager dinosaurManager;
+    // private EmployeeManager employeeManager;
+    // private TicketManager ticketManager;
+
+    public Park(String name) {
+        this.name = name;
+        this.isParkOpen = false;
+        // this.dinosaurManager = new DinosaurManager();
+        // this.employeeManager = new EmployeeManager();
+        // this.ticketManager = new TicketManager();
+    }
+
+    public void openPark() {
+        isParkOpen = true;
+    }
+
+    public void closePark() {
+        isParkOpen = false;
+    }
+
+    // public DinosaurManager getDinosaurManager() {
+    //     return dinosaurManager;
+    // }
+
+    // public EmployeeManager getEmployeeManager() {
+    //     return employeeManager;
+    // }
+
+    // public TicketManager getTicketManager() {
+    // return ticketManager;
+    // }
+}
